ede9632073e70d9b32d725d5c51ee31437c628a9
nourassem98 <nourassem55@yahoo.com>
2023-03-28 15:52:53 +0200

    'npur'

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import jwtDecode from "jwt-decode";
 import { useEffect } from "react";
 import './App.css';
 import Home from './Components/Home/Home';
-import { Routes , Route,useNavigate, Navigate } from 'react-router-dom';
+import { Routes, Route, useNavigate, Navigate } from 'react-router-dom';
 import About from './Components/About/About';
 import Navbar from './Components/Navbar/Navbar';
 import Contact from './Components/Contact/Contact';
@@ -28,7 +28,7 @@ import Login from "./Components/Login/Login";
 import Cart from './Components/Cart/Cart';
 import Profile from './Components/Profile/Profile';
 function App() {
-  const [ar, setAr] =  useState(localStorage.getItem('lang') ? localStorage.getItem('lang') : "eng");
+  const [ar, setAr] = useState(localStorage.getItem('lang') ? localStorage.getItem('lang') : "eng");
   const [userData, setUserData] = useState(null);
   let navigate = useNavigate();
 
@@ -60,83 +60,84 @@ function App() {
   }
   return (
     <>
-      { ar === 'frc' && < NavbarFrance setAr={setAr}/> }
+      {ar === 'frc' && < NavbarFrance setAr={setAr} />}
 
-{  ar === 'eng' && < Navbar setAr={setAr}/>}
-    {/* <Navbar/> */}
-    <MediaContextProvider>
-<Routes>
-{ar ==='frc'&&
-  <>
- {/* <Route  path='navbarAr' element={<NavbarAr/>}/> */}
- 
- <Route path='/about' element={
- <ProtectedRoute>
- <AboutFrance/></ProtectedRoute>}/>
+      {ar === 'eng' && < Navbar setAr={setAr} />}
+      {/* <Navbar/> */}
+      <MediaContextProvider>
+        <Routes>
+          {ar === 'frc' &&
+            <>
+              {/* <Route  path='navbarAr' element={<NavbarAr/>}/> */}
 
-
-  </>
-
-}
-{ar ==='eng'&&
- <>
+              <Route path='/about' element={
+                <ProtectedRoute>
+                  <AboutFrance /></ProtectedRoute>} />
 
 
-<Route path='/' element={<Home/>}/>
-<Route path='/about' element={
-  // <ProtectedRoute>
-<About/>
-// </ProtectedRoute>
-}/>
-<Route path='/contact' element={
-  // <ProtectedRoute>
-<Contact/>
-// </ProtectedRoute>
-}/>
-<Route path='/terms' element={
-  // <ProtectedRoute>
-<Terms/>
-// </ProtectedRoute>
-}/>
-<Route path='/teachers' element={
-  // <ProtectedRoute>
-<Teachers/>
-// </ProtectedRoute>
-}/>
-<Route path='/classes' element={
-  // <ProtectedRoute>
-<Classes/>
-// </ProtectedRoute>
-}/>
-<Route path='/singleClass' element={
-  // <ProtectedRoute>
-<SingleClass/>
-// </ProtectedRoute>
-}/>
-<Route path='/singleTeacher' element={
-  // <ProtectedRoute>
-<SingleTeacher/>
-// </ProtectedRoute>
-}/>
+            </>
 
-<Route path='cart' element={ <Cart/> }/>
-<Route path='/profile' element={ <Profile/> }/>
-
-<Route
-              path="/login"
-              element={<Login getUserData={getUserData} />}/>
-  {/* 
- <Route path="/register" element={
-           
-           <Register />}/> */}
-</>}
+          }
+          {ar === 'eng' &&
+            <>
 
 
-</Routes>
-</MediaContextProvider>
-{ar ==='frc'&&  <FooterFrance/>}
- {  ar=== 'eng'&& <Footer /> }
-</>
+              <Route path='/' element={<Home />} />
+              <Route path='/about' element={
+                // <ProtectedRoute>
+                <About />
+                // </ProtectedRoute>
+              } />
+              <Route path='/contact' element={
+                // <ProtectedRoute>
+                <Contact />
+                // </ProtectedRoute>
+              } />
+              <Route path='/terms' element={
+                // <ProtectedRoute>
+                <Terms />
+                // </ProtectedRoute>
+              } />
+              <Route path='/teachers' element={
+                // <ProtectedRoute>
+                <Teachers />
+                // </ProtectedRoute>
+              } />
+              <Route path='/classes' element={
+                // <ProtectedRoute>
+                <Classes />
+                // </ProtectedRoute>
+              } />
+              <Route path='/singleClass' element={
+                // <ProtectedRoute>
+                <SingleClass />
+                // </ProtectedRoute>
+              } />
+              <Route path='/singleTeacher' element={
+                // <ProtectedRoute>
+                <SingleTeacher />
+                // </ProtectedRoute>
+              } />
+
+              <Route path='cart' element={<Cart />} />
+              <Route path='/profile' element={<Profile />} />
+
+              <Route
+                path="/login"
+                element={<Login getUserData={getUserData} />} />
+
+              <Route path="/register" element={
+
+                <Register />} />
+            </>
+          }
+
+
+        </Routes>
+      </MediaContextProvider>
+      {ar === 'frc' && <FooterFrance />}
+      {ar === 'eng' && <Footer />}
+    </>
   );
 }
 
